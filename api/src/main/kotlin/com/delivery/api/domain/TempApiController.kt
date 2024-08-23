c7d61bb7e66901c1b5946734d8582dc4578386d9
SEOB <svj7531@naver.com>
2024-08-23 20:05:36 +0900

    feat: Kotlin 적용
    feat: Kotlin 적용

---
+++ api/src/main/kotlin/com/delivery/api/domain/TempApiController.kt
@@ -0,0 +1,15 @@
+package com.delivery.api.domain
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/temp")
+class TempApiController {
+
+    @GetMapping("")
+    fun temp(): String {
+        return "hello kotlin spring boot"
+    }
+}
