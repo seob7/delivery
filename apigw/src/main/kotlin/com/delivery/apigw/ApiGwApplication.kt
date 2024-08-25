f36235f826864d102d6a89372a4bfc75b4421fa2
SEOB <svj7531@naver.com>
2024-08-26 01:40:41 +0900

    feat: Add api gateway module
    feat: Add api gateway module

---
+++ apigw/src/main/kotlin/com/delivery/apigw/ApiGwApplication.kt
@@ -0,0 +1,13 @@
+package com.delivery.apigw
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiGwApplication {
+}
+
+
+fun main(args: Array<String>) {
+    runApplication<ApiGwApplication>(*args)
+}
